5916a316e4a70096d84297ea72eed36b9d6b9c40
Your Name <you@example.com>
2023-09-11 16:31:22 +0100

    PYTHON

---
+++ Pythagoras.py
@@ -0,0 +1,19 @@
+print("Enter the operator you want to exscute by choosing the corresponding numbers... ")
+print("Find the length of A given B and C")
+print("Find the length of B given A and C")
+print("Find the length of A given C and B")
+
+operator = input("Enter operator Number: ")
+lengthA = float(input("Enter length of A: "))
+lengthB = float(input("Enter length of B: : "))
+
+if operator == '1':
+    print(lengthA, "**2",+ , lengthB, "**2")
+
+elif operator == '2':
+    print(firstnum, "-", secnum, "=", firstnum - secnum)
+
+elif operator == '3':
+    print(firstnum, "x", secnum, "=", firstnum * secnum)
+else:
+    print("Invalid Operation")
